10328c4fe9415b450afce3b3edf907980435ce25
Shak49 <shakfeizi@gmali.com>
2022-08-11 09:53:05 -0500

    project's files added

---
+++ LetsWalk_Exp/LetsWalk_Exp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LetsWalk_Exp
+//
+//  Created by Shak Feizi on 8/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LetsWalk_Exp/LetsWalk_Exp/LetsWalk_ExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsWalk_ExpApp.swift
+//  LetsWalk_Exp
+//
+//  Created by Shak Feizi on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LetsWalk_ExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
